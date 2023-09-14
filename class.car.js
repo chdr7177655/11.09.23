a550f7735ec5f07ab7b5c243287d4abaeabe247b
chdr7177655 <138139706+chdr7177655@users.noreply.github.com>
2023-09-14 18:48:37 +0300

    Add files via upload

---
+++ class.car.js
@@ -0,0 +1,44 @@
+class Car {
+    constructor(brand, model, color, year, mileage) {
+      this.brand = brand;
+      this.model = model;
+      this.color = color;
+      this.year = year;
+      this.mileage = mileage;
+    }
+  
+    updateBrand(newBrand) {
+      this.brand = newBrand;
+      this.updateCarDetails();
+    }
+  
+    updateModel(newModel) {
+      this.model = newModel;
+      this.updateCarDetails();
+    }
+  
+    updateColor(newColor) {
+      this.color = newColor;
+      this.updateCarDetails();
+      this.updateCarDrawing();
+    }
+  
+    updateYear(newYear) {
+      this.year = newYear;
+      this.updateCarDetails();
+    }
+  
+    updateMileage(newMileage) {
+      this.mileage = newMileage;
+      this.updateCarDetails();
+    }
+  
+    updateCarDetails() {
+      cars.innerText = `Brand: ${this.brand}\nModel: ${this.model}\nColor: ${this.color}\nYear: ${this.year}\nMileage: ${this.mileage}`;
+    }
+  
+    updateCarDrawing() {
+      cars.className = `car ${this.color}`;
+    }
+  }
+  
--- js.Cahw1r.js
+++ js.Cahw1r.js
@@ -13,73 +13,30 @@ const colorSelect = document.getElementById('colorSelect');
 
 
 
-class Car {
-    constructor(brand, model, color, year, mileage) {
-      this.brand = brand;
-      this.model = model;
-      this.color = color;
-      this.year = year;
-      this.mileage = mileage;
-    }
-  
-    updateBrand(newBrand) {
-      this.brand = newBrand;
-      this.updateCarDetails();
-    }
-  
-    updateModel(newModel) {
-      this.model = newModel;
-      this.updateCarDetails();
-    }
-  
-    updateColor(newColor) {
-      this.color = newColor;
-      this.updateCarDetails();
-      this.updateCarDrawing();
-    }
-  
-    updateYear(newYear) {
-      this.year = newYear;
-      this.updateCarDetails();
-    }
-  
-    updateMileage(newMileage) {
-      this.mileage = newMileage;
-      this.updateCarDetails();
-    }
-  
-    updateCarDetails() {
-      cars.innerText = `Brand: ${this.brand}\nModel: ${this.model}\nColor: ${this.color}\nYear: ${this.year}\nMileage: ${this.mileage}`;
-    }
-  
-    updateCarDrawing() {
-      cars.className = `car ${this.color}`;
-    }
-  }
-  
-  const car = new Car('', '', '', '', '');
 
-  brandInput.addEventListener('input', function() {
-    car.updateBrand(brandInput.value);
-  });
+const car = new Car('', '', '', '', '');
 
-  modelInput.addEventListener('input', function() {
-    car.updateModel(modelInput.value);
-  });
+brandInput.addEventListener('input', function () {
+  car.updateBrand(brandInput.value);
+});
 
-  colorInput.addEventListener('input', function() {
-    car.updateColor(colorInput.value);
-  });
+modelInput.addEventListener('input', function () {
+  car.updateModel(modelInput.value);
+});
 
-  yearInput.addEventListener('input', function() {
-    car.updateYear(yearInput.value);
-  });
+colorInput.addEventListener('input', function () {
+  car.updateColor(colorInput.value);
+});
 
-  mileageInput.addEventListener('input', function() {
-    car.updateMileage(mileageInput.value);
-  });
+yearInput.addEventListener('input', function () {
+  car.updateYear(yearInput.value);
+});
 
-  colorSelect.addEventListener('change', function() {
-    const selectedColor = colorSelect.value;
-    car.updateColor(selectedColor);
-  });
+mileageInput.addEventListener('input', function () {
+  car.updateMileage(mileageInput.value);
+});
+
+colorSelect.addEventListener('change', function () {
+  const selectedColor = colorSelect.value;
+  car.updateColor(selectedColor);
+});
